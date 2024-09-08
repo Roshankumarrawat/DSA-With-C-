1fa67e0106d1f52e59c9f0c7674c67dd8ae443a5
Roshan Kumar Rawat <165247276+Roshankumarrawat@users.noreply.github.com>
2024-09-08 23:39:55 +0530

    first code

---
+++ 01_Basic/02_SumOfTwoNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // code here
+  int a, b;
+  cout << "Enter two integers: ";
+  cin >> a >> b;
+  
+  int sum = a + b;
+
+  cout << "Sum: " << sum << endl;
+
+  return 0;
+}
