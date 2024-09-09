7d9e40446cc644889409517c2272da97b618c7f8
Roshan Kumar Rawat <165247276+Roshankumarrawat@users.noreply.github.com>
2024-09-09 12:27:19 +0530

    Operators complete

---
+++ Operators/01_Operators.cpp
@@ -0,0 +1,56 @@
+// C++ Operators
+// Operators are used to perform operations on variables and values
+
+/* Arithmetic Operators
+Arithmetic operators are used to perform common mathematical operations. */
+
+// Operator	     Name	            Description	                              Example	
+// +	    Addition	        Adds together two values                      x + y	
+// -	    Subtraction	     Subtracts one value from another	              x - y	
+// *    	Multiplication      	Multiplies two values	                  x * y	
+// /	    Division	       Divides one value by another	                  x / y	
+// %	    Modulus	         Returns the division remainder	                  x % y	
+// ++	    Increment	   Increases the value of a variable by 1       	   ++x	
+// --	   Decrement	   Decreases the value of a variable by 1             --x
+
+
+// Assignment Operators
+// Assignment operators are used to assign values to variables.
+
+// Operator	       Example	                        Same As	
+//   =	            x = 5	                          x = 5	
+//   +=	           x += 3                         	x = x + 3	
+//   -=           	x -= 3	                        x = x - 3	
+//   *=           	x *= 3	                        x = x * 3	
+//   /=	            x /= 3	                        x = x / 3	
+//   %=            	x %= 3                       	x = x % 3	
+//   &=           	x &= 3                       	x = x & 3	
+//   |=             x |= 3                          x = x | 3	
+//   ^=	            x ^= 3                       	x = x ^ 3	
+//   >>=          	x >>= 3	                        x = x >> 3	
+//  <<=          	x <<= 3                     	x = x << 3
+
+
+// Comparison Operators
+// Comparison operators are used to compare two values (or variables). 
+// This is important in programming, because it helps us to find answers and make decisions.
+
+//Operator	          Name	                                 Example	
+//  ==            	Equal to	                              x == y	
+//  !=	            Not equal	                              x != y	
+//  >	          Greater than	                              x > y	
+//  <	           Less than	                              x < y	
+//  >=	     Greater than or equal to	                      x >= y	
+//  <=	      Less than or equal to	                          x <= y
+
+
+
+// Logical Operators
+// As with comparison operators, you can also test for true (1) or false (0) values with 
+// logical operators.
+
+
+// Operator	Name	Description	                                            Example	
+// && 	      Logical and	Returns true if both statements are true	      x < 5 &&  x < 10	
+// || 	    Logical or	Returns true if one of the statements is true	     x < 5 || x < 4	
+// !	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
